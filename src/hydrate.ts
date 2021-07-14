78233587ae027ea0b0f3ac1acd73c53aae935ce6
fupeng <fupenglxy@gmail.com>
2021-07-14 10:35:11 +0800

    fix: hydrate no persist store error

--- src/hydrate.ts
+++ src/hydrate.ts
@@ -1,9 +1,9 @@
-import type { IReactionDisposer } from "mobx/lib/internal";
-import { getDefaultModelSchema, serialize, update } from "serializr";
-import { action, reaction } from "mobx";
-import mergeObservables from "./merge-observables";
-import IStorage from "./adapters/storage";
-import LocalStorage from "./adapters/localstorage";
+import type { IReactionDisposer } from 'mobx/lib/internal';
+import { getDefaultModelSchema, serialize, update } from 'serializr';
+import { action, reaction } from 'mobx';
+import mergeObservables from './merge-observables';
+import IStorage from './adapters/storage';
+import LocalStorage from './adapters/localstorage';
 
 export interface CreateHydrateOptions {
   /**
@@ -48,14 +48,9 @@ export interface HydrateResult<T> extends Promise<T> {
  * @param opts {CreateHydrateOptions}
  */
 function create(opts: CreateHydrateOptions = {}) {
-  const {
-    storage = new LocalStorage(),
-    jsonify = true,
-    debounce = 0,
-    sync = true,
-  } = opts;
+  const { storage = new LocalStorage(), jsonify = true, debounce = 0, sync = true } = opts;
   if (!(storage instanceof IStorage)) {
-    throw new Error("storage has to implement `IStorage` all method");
+    throw new Error('storage has to implement `IStorage` all method');
   }
   return function hydrate<T extends Object>(
     /**
@@ -67,9 +62,23 @@ function create(opts: CreateHydrateOptions = {}) {
      */
     store: T,
     initialState: any = {},
-    customArgs: any = {}
+    customArgs: any = {},
   ): HydrateResult<T> {
     const schema = getDefaultModelSchema(store as any);
+    if (!schema) {
+      const logError = () => {
+        console.error('[mobx-persist] No cacheable value');
+      };
+      const promise = Promise.resolve(store) as HydrateResult<T>;
+      promise.rehydrate = () => {
+        logError();
+        return promise;
+      };
+      promise.clear = logError;
+      promise.dispose = logError as IReactionDisposer;
+      logError();
+      return promise;
+    }
 
     function hydration() {
       const promise = storage
@@ -77,33 +86,35 @@ function create(opts: CreateHydrateOptions = {}) {
         .then((d: any) => (!jsonify ? d : JSON.parse(d)))
         .then(
           action(`[mobx-persist ${key}] LOAD_DATA`, (persisted: any) => {
-            if (persisted && typeof persisted === "object") {
+            if (persisted && typeof persisted === 'object') {
               update(
                 schema,
                 store,
                 persisted,
                 (err) => {
                   if (err) {
-                    console.error("[mobx-persist] Mobx update error", err);
+                    console.error('[mobx-persist] Mobx update error', err);
                   }
                 },
-                customArgs
+                customArgs,
               );
             }
             mergeObservables(store, initialState);
             return store;
-          })
+          }),
         ) as HydrateResult<T>;
+
       promise.rehydrate = hydration;
+
       // Multiple tab sync state
       if (sync) {
         storage.onChange = (changeKey) => {
           if (changeKey === key) {
             promise.rehydrate();
+            console.warn(`[mobx-persist] change key ${changeKey}`);
           }
         };
       }
-
       return promise;
     }
 
@@ -112,17 +123,11 @@ function create(opts: CreateHydrateOptions = {}) {
     result.clear = () => {
       storage.removeItem(key);
     };
-
-    if (schema) {
-      result.dispose = reaction(
-        () => serialize(schema, store),
-        (data: any) =>
-          storage.setItem(key, !jsonify ? data : JSON.stringify(data)),
-        { delay: debounce }
-      );
-    } else {
-      console.error("[mobx-persist] No cacheable value");
-    }
+    result.dispose = reaction(
+      () => serialize(schema, store),
+      (data: any) => storage.setItem(key, !jsonify ? data : JSON.stringify(data)),
+      { delay: debounce },
+    );
     return result;
   };
 }
